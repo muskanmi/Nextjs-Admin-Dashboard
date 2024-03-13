b985bbf05943d621017cbc6e8713e5f50cc5d402
Muskan Mittal <mittalmuskan758@gmail.com>
2024-03-13 14:48:01 +0530

    updated code

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Card from '../ui/dashboard/card/card'
+import styles from "../ui/dashboard/dashboard.module.css"
+import Transactions from '../ui/dashboard/transactions/transsaction'
+import Chart from '../ui/dashboard/chart/chart'
+import Rightbar from '../ui/dashboard/rightbar/rightbar'
 
 const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.main}>
+                <div className={styles.cards}>
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+                <Transactions />
+                <Chart />
+            </div>
+            <div className={styles.side}>
+                <Rightbar />
+            </div>
+        </div>
+    )
 }
 
 export default DashboardPage
